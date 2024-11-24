44af328ea4d7b15325e8253ccfb6253ba6511c7c
Andres Orozco <andres.orozco.dev@gmail.com>
2024-11-23 22:11:49 -0500

    create rgister and forget form

--- angular/src/app/feature/auth/pages/forget-password/forget-password.component.ts
+++ angular/src/app/feature/auth/pages/forget-password/forget-password.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
 export class ForgetPasswordComponent {
+
+  forgetForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+  });
+
+  onForget() {
+    console.warn(this.forgetForm.value);
+  }
 
 }
--- angular/src/app/feature/auth/pages/register/register.component.ts
+++ angular/src/app/feature/auth/pages/register/register.component.ts
@@ -18,7 +18,7 @@ export class RegisterComponent {
     password: new FormControl('', Validators.required),
   });
 
-  onLogin() {
+  onRegister() {
     console.warn(this.registerForm.value);
   }
 }
