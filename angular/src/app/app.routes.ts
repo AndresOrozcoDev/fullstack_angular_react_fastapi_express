69489e924591efb0d0c03f20fed4bff2e7e91fcf
Andres Orozco <andres.orozco.dev@gmail.com>
2024-11-30 14:09:34 -0500

    fix bug doker

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadChildren: () => import('./feature/auth/auth.module').then((m) => m.AuthModule), },
-    { path: '', redirectTo: '', pathMatch: 'full' },
     { path: '**', redirectTo: '' },
 ];
--- angular/src/app/feature/auth/auth-routing.module.ts
+++ angular/src/app/feature/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
