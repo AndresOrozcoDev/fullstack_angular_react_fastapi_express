4374cbd5c16dc618071d4f31c7407e635de36044
Andres Orozco <andres.orozco.dev@gmail.com>
2024-12-06 18:33:49 -0500

    install and update dev

--- angular/src/app/feature/auth/pages/forget-password/forget-password.component.ts
+++ angular/src/app/feature/auth/pages/forget-password/forget-password.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 
@@ -17,14 +18,16 @@ export class ForgetPasswordComponent {
     username: new FormControl('', Validators.required),
   });
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
 
   onForget() {
     const username = this.forgetForm.value.username || '';
     if (username) {
       this.router.navigate(['']);
+      this.toastr.success('Operación exitosa', 'Éxito');
     } else {
       console.error('Form empty:', this.forgetForm.value);
+      this.toastr.error('Error', 'Error');
     }
   }
 
